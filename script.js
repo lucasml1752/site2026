1ddc3a4e1904b4ef1c6522443021a63461e45d2b
lucasml1752 <lucas.moreira26@escola.pr.gov.br>
2026-06-23 14:08:11 +0000

    finalizar

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-// 1. Mapear os elementos do HTML que vamos usar
-// Buscamos o botão pelo ID 'botao-cor'
-const botao = document.getElementById('botao-cor');
+const botaoTema = document.getElementById("botao-tema");
 
-// 2. Lista de cores que o site pode assumir (Array)
-// Os alunos podem adicionar novos códigos de cores hexadecimais aqui!
-const cores = ['#F5A623', '#E24A4A', '#9B59B6', '#34495E', '#1ABC9C', '#f4f7f6'];
-let indiceCor = 0;
-
-// 3. Criar a função que muda a cor de fundo do body
-function mudarCorDeFundo() {
-    // Aplica a cor atual do array ao estilo do body
-    document.body.style.backgroundColor = cores[indiceCor];
-    
-    // Avança para a próxima cor da lista
-    indiceCor++;
-    
-    // Se chegar ao fim da lista de cores, volta para a primeira (zero)
-    if (indiceCor >= cores.length) {
-        indiceCor = 0;
-    }
-}
-
-// 4. Ouvir o clique do usuário
-// Quando o usuário CLICAR no botão, a função 'mudarCorDeFundo' será executada
-botao.addEventListener('click', mudarCorDeFundo);
+botaoTema.addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+});
